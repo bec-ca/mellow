79017423fa81342b4aed76a065f2c725150e2871
Rebecca Ribas <beky.ribas@gmail.com>
2023-03-08 23:40:50 -0500

    add yasf dep

--- ci.sh
+++ ci.sh
@@ -3,6 +3,7 @@
 ./download-pkg.sh bee https://github.com/bec-ca/bee/archive/refs/tags/v1.0.0.tar.gz
 ./download-pkg.sh command https://github.com/bec-ca/command/archive/refs/tags/v1.0.0.tar.gz
 ./download-pkg.sh diffo https://github.com/bec-ca/diffo/archive/refs/tags/v1.0.0.tar.gz
+./download-pkg.sh yasf https://github.com/bec-ca/yasf/archive/refs/tags/v1.0.0.tar.gz
 
 for file in mellow/*.cpp; do
   echo "Compiling $file..."
