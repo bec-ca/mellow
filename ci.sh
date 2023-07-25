70c58c4326d127af5160a7e3f66af2767c8f2926
Rebecca H Ribas <beky.ribas@gmail.com>
2023-07-24 23:47:13 -0400

    download clang-format

--- ci.sh
+++ ci.sh
@@ -1,11 +1,16 @@
 #!/bin/bash -eu
 
+base_name=clang+llvm-16.0.0-x86_64-linux-gnu-ubuntu-18.04
+archive_name=$base_name.tar.xz
+curl -L -s --show-error "https://github.com/llvm/llvm-project/releases/download/llvmorg-16.0.0/$archive_name"
+tar -xf "$archive_name"
+
+export CLANG_FORMAT="$base_name/bin/clang-format"
+
+"$CLANG_FORMAT" --version
+
 MELLOW=$(./find-mellow.sh)
-
 echo "Mellow command: $MELLOW"
-
-compgen -c
-export CLANG_FORMAT=clang-format16
 
 $MELLOW fetch
 $MELLOW build
