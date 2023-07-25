35e54b84897ac2efffaa62e9fe00a6c7e10fb8ea
Rebecca H Ribas <beky.ribas@gmail.com>
2023-07-24 23:55:15 -0400

    output to file

--- ci.sh
+++ ci.sh
@@ -3,7 +3,7 @@
 echo "Downloading clang 16..."
 base_name=clang+llvm-16.0.0-x86_64-linux-gnu-ubuntu-18.04
 archive_name=$base_name.tar.xz
-curl -L -s --show-error "https://github.com/llvm/llvm-project/releases/download/llvmorg-16.0.0/$archive_name"
+curl -L -s --show-error -o "$archive_name" "https://github.com/llvm/llvm-project/releases/download/llvmorg-16.0.0/$archive_name"
 tar -xf "$archive_name"
 
 export CLANG_FORMAT="$base_name/bin/clang-format"
