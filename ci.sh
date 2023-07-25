e30d2aef677c22a6e511b06e3ca9a5e53d12ab0e
Rebecca H Ribas <beky.ribas@gmail.com>
2023-07-25 00:21:54 -0400

    use apt install

--- ci.sh
+++ ci.sh
@@ -1,12 +1,6 @@
 #!/bin/bash -eu
 
-echo "Downloading clang 16..."
-base_name=clang+llvm-16.0.0-x86_64-linux-gnu-ubuntu-18.04
-archive_name=$base_name.tar.xz
-curl -L -s --show-error -o "$archive_name" "https://github.com/llvm/llvm-project/releases/download/llvmorg-16.0.0/$archive_name"
-tar -xf "$archive_name"
-
-export CLANG_FORMAT="$base_name/bin/clang-format"
+export CLANG_FORMAT="clang-format-16"
 
 "$CLANG_FORMAT" --version
 
