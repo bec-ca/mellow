6a2950530bce36e747947596539e523c060a58be
Rebecca H Ribas <beky.ribas@gmail.com>
2023-07-24 23:47:46 -0400

    log

--- ci.sh
+++ ci.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -eu
 
+echo "Downloading clang 16..."
 base_name=clang+llvm-16.0.0-x86_64-linux-gnu-ubuntu-18.04
 archive_name=$base_name.tar.xz
 curl -L -s --show-error "https://github.com/llvm/llvm-project/releases/download/llvmorg-16.0.0/$archive_name"
